1aaf66333d1c4d689ff46bbc8aa2033a5d10dd75
smgvasya <deepxp@yandex.ru>
2022-12-26 18:55:37 +0300

    second

--- src/components/header/app-header.jsx
+++ src/components/header/app-header.jsx
@@ -16,23 +16,30 @@ class AppHeader extends React.Component {
 
     render() {
         return (
-         <div> 
-            <nav>
-               <div>
-                  <BurgerIcon type="primary"/>
-                  <span className="text text_type_main-default" >Конструктор</span>
-                  <ListIcon type="primary"/>
-                  <span className="text text_type_main-default" >Лента заказов</span>
+        <header className={ styles.header } > 
+            <nav className={ styles.content }>
+               <div className={ styles.navigation }>
+                  <a href="#" className={`pt-4 pr-5 pb-4 pl-5 mr-2 ${styles.link}`} >
+                    <BurgerIcon type="primary"/>
+                    <span className="pl-2 text text_type_main-default " >Конструктор</span>
+                  </a>
+
+                  <a href="#" className={`pt-4 pr-5 pb-4 pl-5 ${styles.link}`} >
+                  <ListIcon type="secondary"/>
+                  <span className="pl-2 text text_type_main-default text_color_inactive " >Лента заказов</span>
+                  </a>
                </div>
 
-               <Logo />
+               <Logo className={ styles.logo } />
 
-               <div>
-                  <ProfileIcon type="primary"/>
-                  <span className="text text_type_main-default" >Личный кабинет</span>
+               <div className={ styles.profile } >
+                  <a href="#" className={`pt-4 pr-5 pb-4 pl-5 ${styles.link}`} >
+                    <ProfileIcon type="secondary"/>
+                    <span className="pl-2 text text_type_main-default text_color_inactive" >Личный кабинет</span>
+                  </a>
                </div>
             </nav>
-         </div>
+         </header>
         );
       }
 }

c557cd25c5bbb3c93bce6fff0c075c8ec3203745
smgvasya <deepxp@yandex.ru>
2022-12-27 23:32:58 +0300

    section

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import styles from './app.module.css';
 import AppHeader from '../header/app-header';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients'
 
 class App extends React.Component {
   render (){
     return (
       <div className="App">
-        <header className="">
         <AppHeader />
+          <div className={styles.main} >
+          <BurgerIngredients />
+          </div>
           {/* <img src={} className="" alt="" />
           <p></p>
           <a></a> */}
-        </header>
       </div>
     );
   }
---
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './burger-ingredients.module.css';
+import {
+    CurrencyIcon,
+    Typography,
+    Tab,
+    Box,
+    Counter,
+
+} from '@ya.praktikum/react-developer-burger-ui-components';
+
+class BurgerIngredients extends React.Component {
+    render() {
+        return (
+            <section>
+           
+
+
+            </section>
+        )
+    }
+}
+
+export default BurgerIngredients
+
+//   <Counter count={1} size="default" extraClass="m-1" /> 

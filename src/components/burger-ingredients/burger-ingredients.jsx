392910a2db8c0f979421ce5fe0f245c0d5d010f2
smgvasya <deepxp@yandex.ru>
2023-01-07 03:11:24 +0300

    style-ingr

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -10,12 +10,14 @@ import Ingredients from './ingredients'
 
 const BurgerIngredients = () => {
      return (
-        <section>
+        <section className={ styles.section }>
             <h1 className="text text_type_main-large pb-5" >Соберите бургер</h1>
             <IngredientsTabs />
+            <div  className={ styles.ingredient }>
             <Ingredients type='bun' title='Булки' />
             <Ingredients type='sauce' title='Соусы' />
             <Ingredients type='main' title='Начинки' />
+            </div>
         </section>  
         )
 
--- src/components/burger-ingredients/ingredients.jsx
+++ src/components/burger-ingredients/ingredients.jsx
@@ -15,22 +15,22 @@ const Ingredients = ({title, type}) => {
     const content = useMemo(() => data.filter((item) => item.type === type)) ;
 
         return (
-           <div className={ styles.grid }>
-            <h2 className='text text_type_main-medium pb-6' >{title}</h2>
-                <ul className={ styles.list } >
+            <>
+            <h2 className='text text_type_main-medium pb-6 pt-10' >{title}</h2>
+                <ul className={ styles.list }  >
                     {content.map ((item) => (
-                        <li className='pr-4 pl-4' type={type} key={item._id}>
+                        <li type={type} key={item._id}>
                         <Counter count={1} size="default" extraClass="m-1" />
-                        <img className='pt-6 pb-10' src={item.image} alt={item.name} />
-                        <div>
+                        <img className='pr-4 pb-1 pl-4' src={item.image} alt={item.name} />
+                        <div className={ styles.price } >
                         <span className="pt-1 pb-1 text text_type_digits-default">{item.price}</span>
                         <CurrencyIcon type="primary" />
                         </div>
-                        <p className="pt-1 pb-1 text text_type_digits-default">{item.name}</p>
+                        <p className={`pt-1 pb-1 text text text_type_main-small ${ styles.name }`}>{item.name}</p>
                     </li>
                     ))}
                 </ul>
-           </div>
+            </>
         )
 }
 

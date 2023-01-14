a0446c1ffeaee58443d98ae3c815ddb04666152c
smgvasya <deepxp@yandex.ru>
2023-01-14 22:29:37 +0300

    for-rew

--- src/components/modal/modal-overlay.jsx
+++ src/components/modal/modal-overlay.jsx
@@ -1,15 +1,10 @@
-import React from 'react';
 import styles from './modal.module.css';
 import {modalPropTypes} from '../../utils/propTypes';
 
 const ModalOverlay = ({children, visible }) => {
 
-  const rootClasses = [styles.overlay]
-    visible && rootClasses.push(styles.active);
-
-
   return (
-    <div className={rootClasses.join(' ')} onClick={() => visible(false)} >
+    <div className={styles.overlay} onClick={() => visible(false)} >
       {children}
     </div>
   )

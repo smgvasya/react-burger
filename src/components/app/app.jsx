13dddede06e65a7aca8b3bebe7c7e3c037be24ef
smgvasya <deepxp@yandex.ru>
2023-04-14 01:23:25 +0300

    DndProvider

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect, useMemo } from "react";
 import styles from "./app.module.css";
 
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 import { getIngredients } from "../../services/actions/burger-ingredients";
 import AppHeader from "../header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
@@ -9,7 +12,7 @@ import BurgerConstructor from "../burger-constructor/burger-constructor";
 import { useDispatch, useSelector } from "react-redux";
 
 const App = () => {
-  const { dataRequest } = useSelector((state) => state.ingredients.data);
+  const loading = useSelector((state) => state.ingredients.loaded);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,14 +22,16 @@ const App = () => {
   return (
     <>
       <AppHeader />
-      {dataRequest ? (
-        <h1 className={`${styles.loading} text text_type_digits-large`}>
+      {loading ? (
+        <h1 className={`${styles.loading} text text_type_digits-medium`}>
           Загрузка..
         </h1>
       ) : (
         <main className={styles.main}>
+          <DndProvider backend={HTML5Backend}>
           <BurgerIngredients />
           <BurgerConstructor />
+          </DndProvider>
         </main>
       )}
     </>

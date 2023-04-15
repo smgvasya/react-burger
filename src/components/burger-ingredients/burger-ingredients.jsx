a862c38217aa12a7d571142bb2fb68b07674747b
smgvasya <deepxp@yandex.ru>
2023-04-16 02:50:07 +0300

    useDrag

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useDrag } from "react-dnd";
 import styles from "./burger-ingredients.module.css";
 import IngredientsTabs from "./ingredients-tabs";
 import Ingredients from "./ingredients";
@@ -16,6 +17,7 @@ const BurgerIngredients = () => {
   const dispatch = useDispatch();
 
   const ingredientsModal = useSelector((state) => state.ingredientDetails.data);
+  const ingredients = useSelector((state) => state.ingredients.data);
 
   const handleCloseModal = () => {
     dispatch(detailsClose());
@@ -46,6 +48,15 @@ const BurgerIngredients = () => {
 
   }, [dispatch]);
 
+
+  const [{ opacity }, dragRef] = useDrag({
+    type: 'ingredient',
+    item: { ...ingredients },
+    collect: monitor => ({
+      opacity: monitor.isDragging() ? 0 : 1
+    })
+  });
+
   return (
     <section className={styles.section}>
       <h1 className="text text_type_main-large pb-5 mt-10">Соберите бургер</h1>
@@ -55,18 +66,24 @@ const BurgerIngredients = () => {
           type="bun"
           title="Булки"
           id={"bun"}
+          ref={dragRef}
+          style={{ opacity }}
           getData={handleOpenModal}
         />
         <Ingredients
           type="sauce"
           title="Соусы"
           id={"sause"}
+          ref={dragRef}
+          style={{ opacity }}
           getData={handleOpenModal}
         />
         <Ingredients
           type="main"
           title="Начинки"
           id={"main"}
+          ref={dragRef}
+          style={{ opacity }}
           getData={handleOpenModal}
         />
       </div>

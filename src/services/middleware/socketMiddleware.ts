8dd30acf9e85d3bde90b3b3ce0f994c9bed32089
smgvasya <deepxp@yandex.ru>
2023-06-30 14:37:08 +0300

    2

--- src/services/middleware/socketMiddleware.ts
+++ src/services/middleware/socketMiddleware.ts
@@ -1,11 +1,10 @@
 import { getCookie } from "../../utils/cookie";
 import { updateToken } from "../actions/user";
-import { MiddlewareAPI, Middleware } from "redux";
+import { MiddlewareAPI } from "redux";
 import { MiddlewareTypes } from "../types/types";
 
 export const socketMiddleware =
-  (wsUrl: string, wsActions: MiddlewareTypes): Middleware =>
-  (store: MiddlewareAPI) => {
+  (wsUrl: string, wsActions: MiddlewareTypes) => (store: MiddlewareAPI) => {
     let socket: WebSocket | null = null;
 
     return (next: any) => (action: { type: string }) => {
